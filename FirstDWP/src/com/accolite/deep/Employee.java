5de53107e6dc83d39d3f5de224e14fed2d2743bb
Saumyadeep <saumyadeep.tarafdar@accoliteindia.com>
2016-07-07 12:12:57 +0530

    Used jautodoc

---
+++ FirstDWP/src/com/accolite/deep/Employee.java
@@ -0,0 +1,72 @@
+package com.accolite.deep;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Employee.
+ */
+public class Employee {
+	
+	/** The id. */
+	private int id;
+	
+	/** The name. */
+	private String name;
+	
+	/** The dept. */
+	private String dept;
+	
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets the dept.
+	 *
+	 * @return the dept
+	 */
+	public String getDept() {
+		return dept;
+	}
+	
+	/**
+	 * Sets the dept.
+	 *
+	 * @param dept the new dept
+	 */
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+}
